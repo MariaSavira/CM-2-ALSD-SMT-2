cc7204aee8d96717088043e2708ae7ad4b4dee41
MariaSavira <mariasavira2006@gmail.com>
2025-05-29 14:54:48 +0700

    Class Awal

---
+++ Kendaraan.java
@@ -0,0 +1,15 @@
+public class Kendaraan{
+    String platNomor;
+    String tipe;
+    String merk;
+
+    public Kendaraan(String platNomor, String tipe, String merk) {
+        this.platNomor = platNomor;
+        this.tipe = tipe;
+        this.merk = merk;
+    }
+
+    public void tampilkanInformasi(){
+        System.out.println("Plat Nomor : " + platNomor +"/tJenis Kendaraan : " + tipe + "/tMerk : " + merk);
+    }
+}
